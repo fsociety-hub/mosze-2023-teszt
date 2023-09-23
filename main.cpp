1854043aad68a2c851378b1f32db5ffe30fa72e4
Pető Martin <84993958+fsociety-hub@users.noreply.github.com>
2023-09-23 13:47:34 +0200

    hibák kommentelése

--- main.cpp
+++ main.cpp
@@ -5,22 +5,23 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    std::cout << '1-100 ertekek duplazasa' // ; elmaradás
+    for (int i = 0;) // hiányos for ciklus szintakszis
     {
-        b[i] = i * 2;
+        b[i] = i * 2; // int tipusu j valtozo felvetele(kezdőérték=0), duplázásával kapjuk majd a tömb elemeit
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // hiányos for ciklus szintakszis
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // ; elmaradás
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] // ; elmaradás
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    // memória nincs felszabadítva
     return 0;
 }
