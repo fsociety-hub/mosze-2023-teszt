93ce1218cfdc437ddc41f41b767ac14c4199e746
Pető Martin <84993958+fsociety-hub@users.noreply.github.com>
2023-09-23 13:50:10 +0200

    Javított hibák: szintaktikai hibák(elmaradt ';', nem teljes for ciklus) javítása,+1 változó, memória felszabadítás

--- main.cpp
+++ main.cpp
@@ -2,26 +2,28 @@
 
 constexpr int N_ELEMENTS = 100;
 
-int main()
-{
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa' // ; elmaradás
-    for (int i = 0;) // hiányos for ciklus szintakszis
-    {
-        b[i] = i * 2; // int tipusu j valtozo felvetele(kezdőérték=0), duplázásával kapjuk majd a tömb elemeit
-    }
-    for (int i = 0; i; i++) // hiányos for ciklus szintakszis
-    {
-        std::cout << "Ertek:" // ; elmaradás
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
-    {
-        atlag += b[i] // ; elmaradás
-    }
-    atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
-    // memória nincs felszabadítva
-    return 0;
+int main() {
+  int *b = new int[N_ELEMENTS];
+  // int osszeg = 0;
+  int j = 1;
+  std::cout << "1-100 ertekek duplazasa\n";
+  for (int i = 0; i < N_ELEMENTS; i++) {
+    b[i] = j * 2;
+    // osszeg += b[i];
+    j++;
+  }
+  for (int i = 0; i < N_ELEMENTS; i++) {
+    std::cout << "Ertek: " << b[i] << " " << std::endl;
+  }
+  // std::cout << "Összeg: " << osszeg << std::endl;
+  std::cout << "\nAtlag szamitasa: " << std::endl;
+  int atlag;
+  for (int i = 0; i < N_ELEMENTS; i++) {
+    atlag += b[i];
+  }
+  atlag /= N_ELEMENTS;
+  std::cout << "Atlag: " << atlag << std::endl;
+  delete[] b;
+  std::cout <<"Program vege.";
+  return 0;
 }
